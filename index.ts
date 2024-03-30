2e6094727972c60b80e4539311033fb7397631cb
safderjamal <safderj2@gmail.com>
2024-03-31 02:07:14 +0500

    Fixed the bugs and added some features in Calculator:

--- index.ts
+++ index.ts
@@ -1,23 +1,40 @@
-#! usr/bin/env node
+#! /usr/bin/env node
 //Shabang
 
 import inquirer from "inquirer";
+console.log ("\n=======================================\n");
+console.log ("<<<<<<-- CREATED BY SAFDAR JAMAL -->>>>>>");
+console.log ("/*/*/*/*/ SIMPLE CALCULATOR /*/*/*/*/\n");
+
+const action = "Your Answer is : "
 
 const answer = await inquirer.prompt([
-    {message: "Enter First Number", type : "number", name: "FirstNumber"},
-    {message: "Enter Second Number", type : "number", name: "SecondNumber"},
-    {message: "Please select one of the operator to perform action", type : "list", name: "operator", 
-    choices:["Addition", "Subtraction","Multiplication", "Division" ],},
+  { message: "Enter Your First Number =", type: "number", name: "FirstNumber" },
+  { message: "Enter Your Second Number =", type: "number", name: "SecondNumber" },
+  {
+    message: "Please Select the Operator",
+    type: "list",
+    name: "operator",
+    choices: ["Addition", "Subtraction", "Multiplication", "Division"],
+  },
 ]);
 
-console.log (answer);
+console.log(answer);
 
-if(answer.operator === "Addition")
-{console.log (answer.FirstNumber + answer.SecondNumber);}
-else if(answer.operator === "Subtraction")
-{console.log (answer.FirstNumber - answer.SecondNumber);}
-else if(answer.operator === "Multiplication")
-{console.log (answer.FirstNumber * answer.SecondNumber);}
-else if(answer.operator === "Division")
-{console.log (answer.FirstNumber / answer.SecondNumber);}
-else {console.log("Please select the Valid Operator")}
+if (answer.operator === "Addition") {
+    console.log (answer.FirstNumber, " + ", answer.SecondNumber)
+      console.log(action , answer.FirstNumber + answer.SecondNumber);
+} else if (answer.operator === "Subtraction") {
+    console.log (answer.FirstNumber, " - ", answer.SecondNumber)
+  console.log(action , answer.FirstNumber - answer.SecondNumber);
+} else if (answer.operator === "Multiplication") {
+    console.log (answer.FirstNumber, " x ", answer.SecondNumber)
+  console.log(action , answer.FirstNumber * answer.SecondNumber);
+} else if (answer.operator === "Division") {
+    console.log (answer.FirstNumber, " / ", answer.SecondNumber)
+  console.log(action , answer.FirstNumber / answer.SecondNumber);
+} else {
+  console.log("Please Select the Valid Operator");
+}
+
+console.log ("\n=======================================");
