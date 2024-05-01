b38329d7fc5e1be1a394a0230316fd32d7164302
safderjamal <safderj2@gmail.com>
2024-05-01 05:43:49 +0500

    Added embedded expression and Loop

--- index.js
+++ index.js
@@ -1,38 +1,56 @@
 #! /usr/bin/env node
-//Shabang
+//Shebang
 import inquirer from "inquirer";
 console.log("\n=======================================\n");
 console.log("<<<<<<-- CREATED BY SAFDAR JAMAL -->>>>>>");
 console.log("/*/*/*/*/ SIMPLE CALCULATOR /*/*/*/*/\n");
-const action = "Your Answer is : ";
-const answer = await inquirer.prompt([
-    { message: "Enter Your First Number =", type: "number", name: "FirstNumber" },
-    { message: "Enter Your Second Number =", type: "number", name: "SecondNumber" },
-    {
-        message: "Please Select the Operator",
-        type: "list",
-        name: "operator",
-        choices: ["Addition", "Subtraction", "Multiplication", "Division"],
-    },
-]);
-console.log(answer);
-if (answer.operator === "Addition") {
-    console.log(answer.FirstNumber, " + ", answer.SecondNumber);
-    console.log(action, answer.FirstNumber + answer.SecondNumber);
-}
-else if (answer.operator === "Subtraction") {
-    console.log(answer.FirstNumber, " - ", answer.SecondNumber);
-    console.log(action, answer.FirstNumber - answer.SecondNumber);
-}
-else if (answer.operator === "Multiplication") {
-    console.log(answer.FirstNumber, " x ", answer.SecondNumber);
-    console.log(action, answer.FirstNumber * answer.SecondNumber);
-}
-else if (answer.operator === "Division") {
-    console.log(answer.FirstNumber, " / ", answer.SecondNumber);
-    console.log(action, answer.FirstNumber / answer.SecondNumber);
-}
-else {
-    console.log("Please Select the Valid Operator");
+let condition = true;
+while (condition) {
+    //Input from User
+    const answer = await inquirer.prompt([
+        { message: "Enter Your First Number =", type: "number", name: "FirstNumber" },
+        { message: "Enter Your Second Number =", type: "number", name: "SecondNumber" },
+        {
+            message: "Please Select the Operator",
+            type: "list",
+            name: "operator",
+            choices: ["Addition", "Subtraction", "Multiplication", "Division"],
+        },
+    ]);
+    console.log(answer);
+    //Apply conditions if select the Operator
+    if (answer.operator === "Addition") {
+        console.log(`${answer.FirstNumber} + ${answer.SecondNumber}`);
+        console.log(`Your Answer is : ${answer.FirstNumber + answer.SecondNumber}\n`);
+    }
+    else if (answer.operator === "Subtraction") {
+        console.log(`${answer.FirstNumber} - ${answer.SecondNumber}`);
+        console.log(`Your Answer is : ${answer.FirstNumber - answer.SecondNumber}\n`);
+    }
+    else if (answer.operator === "Multiplication") {
+        console.log(`${answer.FirstNumber} x ${answer.SecondNumber}`);
+        console.log(`Your Answer is : ${answer.FirstNumber * answer.SecondNumber}\n`);
+    }
+    else if (answer.operator === "Division") {
+        console.log(`${answer.FirstNumber} / ${answer.SecondNumber}`);
+        console.log(`Your Answer is : ${answer.FirstNumber / answer.SecondNumber}\n`);
+    }
+    else {
+        console.log("Please Select the Valid Operator");
+    }
+    //Apply Loop
+    const calculation = await inquirer.prompt({
+        message: "Do you want to Calculate more ?",
+        type: "confirm",
+        name: "calculate",
+        default: false
+    });
+    if (calculation.calculate == true) {
+        condition = true;
+        console.log("\t");
+    }
+    else {
+        condition = false;
+    }
 }
 console.log("\n=======================================");
--- index.ts
+++ index.ts
@@ -1,12 +1,14 @@
 #! /usr/bin/env node
-//Shabang
+//Shebang
 
 import inquirer from "inquirer";
 console.log ("\n=======================================\n");
 console.log ("<<<<<<-- CREATED BY SAFDAR JAMAL -->>>>>>");
 console.log ("/*/*/*/*/ SIMPLE CALCULATOR /*/*/*/*/\n");
 
-const action = "Your Answer is : "
+let condition = true;
+while (condition){
+//Input from User
 
 const answer = await inquirer.prompt([
   { message: "Enter Your First Number =", type: "number", name: "FirstNumber" },
@@ -21,20 +23,41 @@ const answer = await inquirer.prompt([
 
 console.log(answer);
 
+//Apply conditions if select the Operator
+
 if (answer.operator === "Addition") {
-    console.log (answer.FirstNumber, " + ", answer.SecondNumber)
-      console.log(action , answer.FirstNumber + answer.SecondNumber);
+    console.log (`${answer.FirstNumber} + ${answer.SecondNumber}`)
+      console.log(`Your Answer is : ${answer.FirstNumber + answer.SecondNumber}\n`);
 } else if (answer.operator === "Subtraction") {
-    console.log (answer.FirstNumber, " - ", answer.SecondNumber)
-  console.log(action , answer.FirstNumber - answer.SecondNumber);
+  console.log (`${answer.FirstNumber} - ${answer.SecondNumber}`)
+  console.log(`Your Answer is : ${answer.FirstNumber - answer.SecondNumber}\n`);
 } else if (answer.operator === "Multiplication") {
-    console.log (answer.FirstNumber, " x ", answer.SecondNumber)
-  console.log(action , answer.FirstNumber * answer.SecondNumber);
+  console.log (`${answer.FirstNumber} x ${answer.SecondNumber}`)
+  console.log(`Your Answer is : ${answer.FirstNumber * answer.SecondNumber}\n`);
 } else if (answer.operator === "Division") {
-    console.log (answer.FirstNumber, " / ", answer.SecondNumber)
-  console.log(action , answer.FirstNumber / answer.SecondNumber);
+  console.log (`${answer.FirstNumber} / ${answer.SecondNumber}`)
+  console.log(`Your Answer is : ${answer.FirstNumber / answer.SecondNumber}\n`);
 } else {
   console.log("Please Select the Valid Operator");
 }
 
+//Apply Loop
+const calculation = await inquirer.prompt({
+  message: "Do you want to Calculate more ?",
+  type: "confirm",
+  name: "calculate",
+ default: false
+})
+if(calculation.calculate == true){
+  condition = true;
+  console.log("\t");
+}
+else {
+  condition = false;
+}
+}
 console.log ("\n=======================================");
+
+
+
+
